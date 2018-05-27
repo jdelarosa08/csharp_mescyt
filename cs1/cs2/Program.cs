203b6fe0e92c0469acf03437c8cf11ad864d4738
Joan De La Rosa <joan_delarosa@intellisys.com.do>
2018-05-27 17:42:35 -0400

    ed

--- cs1/cs2/Program.cs
+++ cs1/cs2/Program.cs
@@ -6,14 +6,19 @@ namespace cs2
     {
         static void Main(string[] args)
         {
+            //var tipo = "par";
             var num = 0;
             string tipo;
 
             Console.WriteLine("digite un numero");
             num = Console.Read();
 
-            tipo = num % 2 == 0 ? num = "par" : num = "impar"; //operador altelnario .... 
+            tipo = (num % 2) == 0 ? "par" : "impar"; //operador altelnario .... 
             Console.WriteLine(tipo);
+            //var  x = 10;
+            //var y = (x > 9) ? 100 : 200;
+            //Console.WriteLine(y);
+
 
         }
     }
